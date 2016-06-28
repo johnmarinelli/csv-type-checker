02bf6cdf841b140382128a8610329b0f972ddbd6
johnmarinelli <john.marinelli.dev@gmail.com>
2016-06-28 16:50:25 -0700

    start to add metadata to types

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-time "0.12.0"]])
--- src/csv_type_checker/core.clj
+++ src/csv_type_checker/core.clj
@@ -1,7 +1,9 @@
 (ns csv-type-checker.core
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [clojure.data.json :as json])
+            [clojure.data.json :as json]
+            [clj-time.core :as time]
+            [clj-time.format :as time-format])
   (:import [clojure.lang PersistentVector PersistentHashMap]))
 
 (def resource-path (str (System/getProperty "user.dir") "/resources/"))
@@ -19,15 +21,25 @@
 ; pure
 (comment(defn try-catch [f & args] (try (apply f args) (catch Exception e (.getMessage e)))))
 
+(defn create-timestamp [input-fmt timestamp]
+  (let [format (time-format/formatter input-fmt)]
+    (time-format/parse format timestamp)))
+
 ; type => { constraints }
 (def types {"String" []
-            "Integer" [(fn [i] (try (Integer. i) (catch Exception e nil)))]
-            "Float" [(fn [f] (try (Double. f) (catch Exception e nil)))]})
+            "Integer" [(fn [i _] (try (Integer. i) (catch Exception e nil)))]
+            "Float" [(fn [f _] (try (Double. f) (catch Exception e nil)))]
+            "Timestamp" [(fn [timestamp metadata] 
+                           (try (create-timestamp (metadata "format") timestamp)
+                                (catch Exception e nil)))]})
 
-(defmulti parse-field (fn [t _] (identity t)))
+(defmulti parse-field (fn [t _] (t "type")))
 (defmethod parse-field "String" [_ v] v)
 (defmethod parse-field "Integer" [_ v] (Integer. v))
 (defmethod parse-field "Float" [_ v] (Float. v))
+(defmethod parse-field "Timestamp" [t v]
+  (let [input-fmt (t "format")]
+    (create-timestamp input-fmt v)))
 
 ; [ { :field_1 => 'field_1_val', :field_2 => 'field_2_val'... } {} {}...]
 (defn transform-csv [^PersistentVector csv]
@@ -37,13 +49,15 @@
 
 (defn validate-cell [^Long row-number ^PersistentHashMap input-types cell]
   (let [field-name (first cell)
-        t (input-types field-name)
+        metadata (input-types field-name)
+        field-type (metadata "type")
         value (second cell)
-        constraints (types t)
-        validated (map #(% value) constraints)]
-    (if (some nil? validated)
-      (println (str "Error found at row " row-number ".  Column: " field-name))
-      (parse-field t value))))
+        constraints (types field-type)]
+    (when (not (= "" value))
+      (let [validated (map #(% value metadata) constraints)]
+        (if (some nil? validated)
+          (println (str "Error found at row " row-number ".  Column: " field-name))
+          (parse-field metadata value))))))
    
 ; `row` should be a transformed-csv row
 (defn validate-row [^PersistentHashMap input-types ^PersistentVector row] 
