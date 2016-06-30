5392493bce09f42e1a5c47c4dfe5092ea82aea6f
johnmarinelli <john.marinelli.dev@gmail.com>
2016-06-30 11:08:59 -0700

    start tests

--- src/csv_type_checker/core.clj
+++ src/csv_type_checker/core.clj
@@ -4,7 +4,7 @@
             [clojure.data.json :as json]
             [clj-time.core :as time]
             [clj-time.format :as time-format]
-            [clojure.string :refer [trim]])
+            [clojure.string :refer [trim lower-case]])
   (:import [clojure.lang PersistentVector PersistentHashMap]))
 
 (def resource-path (str (System/getProperty "user.dir") "/resources/"))
@@ -26,29 +26,33 @@
   (let [format (time-format/formatter input-fmt)]
     (time-format/parse format timestamp)))
 
-(def type-errors {:formatting-error "Format error."})
+(def type-error-definitions {:formatting-error "Format error."})
+(def type-errors (keys type-error-definitions))
+
 
 ; type => { constraints }
-(def types {"String" []
-            "Integer" [(fn [i _] (try (Integer. i) (catch Exception e :formatting-error)))]
-            "Float" [(fn [f _] (try (Double. f) (catch Exception e :formatting-error)))]
-            "Timestamp" [(fn [timestamp metadata]
-                           (try (create-timestamp (metadata "format") timestamp)
-                                (catch Exception e :formatting-error)))]})
+(def type-constraints { "String" []
+                        "Integer" [(fn [i _] (try (Integer. i) (catch Exception e :formatting-error)))]
+                        "Float" [(fn [f _] (try (Double. f) (catch Exception e :formatting-error)))]
+                        "Timestamp" [(fn [timestamp metadata]
+                                       (try (create-timestamp (metadata "format") timestamp)
+                                            (catch Exception e :formatting-error)))]})
 
-(defmulti parse-field (fn [t _] (t "type")))
-(defmethod parse-field "String" [_ v] v)
-(defmethod parse-field "Integer" [_ v] (Integer. v))
-(defmethod parse-field "Float" [_ v] (Float. v))
-(defmethod parse-field "Timestamp" [t v]
-  (let [input-fmt (t "format")]
-    (create-timestamp input-fmt v)))
+;(defmulti parse-typed-value (fn [t _] (lower-case (t "type"))))
+;(defmethod parse-typed-value "string" [_ v] v)
+;(defmethod parse-typed-value "integer" [_ v] (Integer. v))
+;(defmethod parse-typed-value "float" [_ v] (Float. v))
+;(defmethod parse-typed-value "timestamp" [t v]
+;  (let [input-fmt (t "format")]
+;    (create-timestamp input-fmt v)))
+;
 
+; transforms tabular data into associative array.  requires first row to be headers.
 ; [ { :field_1 => 'field_1_val', :field_2 => 'field_2_val'... } {} {}...]
 (defn transform-csv [^PersistentVector csv]
   (let [headers (first csv)
         rows (rest csv)] 
-    (map (fn [row] (apply array-map (interleave headers row))) rows)))
+    (map #(apply array-map (interleave headers %)) rows)))
 
 (defn validate-cell [^Long row-number ^PersistentHashMap input-types cell]
   (let [field-name (first cell)
@@ -56,12 +60,10 @@
         field-type (metadata "type")
         raw-value (second cell)
         value (trim raw-value)
-        constraints (types field-type)]
+        constraints (type-constraints field-type)]
     (when (not (= "" value))
       (let [validated (map #(% value metadata) constraints)
-            a (println validated)
-            possible-type-errors (keys type-errors)
-            errors (clojure.set/intersection (set validated) (set possible-type-errors))]
+            errors (clojure.set/intersection (set validated) (set type-errors))]
         (if (> (count errors) 0) ; if there are any type-error keys in possible-type-errors
           (println (str "Error at row " row-number ", field " field-name))
           (println (str "No errors for row " row-number ", field " field-name)))
