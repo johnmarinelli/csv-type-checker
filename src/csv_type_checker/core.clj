8f67568fe031909bd24adbcf975bd258c6f9caf0
John Marinelli <john.marinelli.dev@gmail.com>
2016-07-04 01:13:04 -0700

    existence & uniqueness

--- src/csv_type_checker/core.clj
+++ src/csv_type_checker/core.clj
@@ -10,6 +10,14 @@
 (def resource-path (str (System/getProperty "user.dir") "/resources/"))
 
 ; impure
+(def ^:dynamic *raw-csv-data* (atom nil))
+(defn get-column [^Long col-idx]
+  (let [
+        raw-csv-data (deref *raw-csv-data*)
+        
+        rows (rest raw-csv-data)]
+    (map #(nth % col-idx) rows)))
+
 (defn read-csv [^String filename]
   (with-open [f (io/reader filename)]
     (doall (csv/read-csv f))))
@@ -29,23 +37,36 @@
 (def type-error-definitions {:formatting-error "Format error."})
 (def type-errors (keys type-error-definitions))
 
-
 ; type => { constraints }
 (def type-constraints { "String" []
-                        "Integer" [(fn [i _] (try (Integer. i) (catch Exception e :formatting-error)))]
-                        "Float" [(fn [f _] (try (Double. f) (catch Exception e :formatting-error)))]
-                        "Timestamp" [(fn [timestamp metadata]
+                        "Integer" [(fn [i _ _] (try (Integer. i) (catch Exception e :formatting-error)))]
+                        "Float" [(fn [f _ _] (try (Double. f) (catch Exception e :formatting-error)))]
+                        "Timestamp" [(fn [timestamp metadata _]
                                        (try (create-timestamp (metadata "format") timestamp)
                                             (catch Exception e :formatting-error)))]})
 
-;(defmulti parse-typed-value (fn [t _] (lower-case (t "type"))))
-;(defmethod parse-typed-value "string" [_ v] v)
-;(defmethod parse-typed-value "integer" [_ v] (Integer. v))
-;(defmethod parse-typed-value "float" [_ v] (Float. v))
-;(defmethod parse-typed-value "timestamp" [t v]
-;  (let [input-fmt (t "format")]
-;    (create-timestamp input-fmt v)))
-;
+(def uniqueness-error-definitions {:uniqueness-error "Uniqueness error."})
+(def uniqueness-errors (keys uniqueness-error-definitions))
+
+(def uniqueness-constraints [(fn [value metadata field-name]
+                               (let [headers (first (deref *raw-csv-data*))
+                                     column-index (.indexOf headers field-name)
+                                     colvals (get-column column-index)
+                                     v (filter #(= % value) colvals)
+                                     v-count (count v)
+                                     unique? (= v-count 1)]
+                                 (if unique? value :uniqueness-error)))])
+
+(def existence-error-definitions {:existence-error "Existence error."})
+(def existence-errors (keys existence-error-definitions))
+
+(def existence-constraints [(fn [value metadata field-name]
+                              (let [check-for-existence? (boolean (Boolean/valueOf (metadata "existence")))]
+                                (if (and check-for-existence? (= value "")) 
+                                  :existence-error
+                                  value)))])
+
+(def all-error-definitions (merge type-error-definitions uniqueness-error-definitions existence-error-definitions))
 
 ; transforms tabular data into associative array.  requires first row to be headers.
 ; [ { :field_1 => 'field_1_val', :field_2 => 'field_2_val'... } {} {}...]
@@ -55,19 +76,23 @@
     (map #(apply array-map (interleave headers %)) rows)))
 
 (defn validate-cell [^Long row-number ^PersistentHashMap input-types cell]
-  (let [field-name (first cell)
+  (let [
+        field-name (first cell)
         metadata (input-types field-name)
         field-type (metadata "type")
         raw-value (second cell)
         value (trim raw-value)
-        constraints (type-constraints field-type)]
-    (when (not (= "" value))
-      (let [validated (map #(% value metadata) constraints)
-            errors (clojure.set/intersection (set validated) (set type-errors))]
-        (if (> (count errors) 0) ; if there are any type-error keys in possible-type-errors
-          (println (str "Error at row " row-number ", field " field-name))
-          (println (str "No errors for row " row-number ", field " field-name)))
-        validated))))
+        constraints (concat uniqueness-constraints existence-constraints (type-constraints field-type))]
+    (let [validated (map #(% value metadata field-name) constraints)
+          filter (clojure.set/union (set type-errors) (set uniqueness-errors) (set existence-errors))
+          errors (clojure.set/intersection (set validated) filter)
+          select-values (comp vals select-keys)
+          error-keys (into [] errors)
+          msgs (select-values all-error-definitions error-keys)]
+      (if (> (count errors) 0) ; if there are any type-error keys in possible-type-errors
+        (println (str "Error at row " row-number ", field " field-name ": " msgs))
+        (println (str "No errors for row " row-number ", field " field-name)))
+      validated)))
    
 ; `row` should be a transformed-csv row
 (defn validate-row [^PersistentHashMap input-types ^PersistentVector row] 
